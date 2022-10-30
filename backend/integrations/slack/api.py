e6248122ac77c8fc770bbf54689769245135607e
sanskargupta <sanskarg348@gmail.com>
2022-10-30 10:05:32 +0530

    Added Slack api integration and created structure for YouTube integration

---
+++ backend/integrations/slack/api.py
@@ -0,0 +1,14 @@
+import os
+from slack_sdk import WebClient
+
+
+def get_users():
+    client = WebClient(token=os.environ.get('SLACK_TOKEN'))
+    result = client.users_list()
+    while result.status_code != 200:
+        result = client.users_list()
+
+    return len(result["members"])
+
+
+
